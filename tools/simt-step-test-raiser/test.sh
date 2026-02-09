a0bce70c53690f6e65d38841818a079e032132de
Simon Kagle <skagle@ucsc.edu>
2026-02-09 14:34:57 -0800

    Added no-wrapper option to not emit Python wrapper when possible

--- tools/simt-step-test-raiser/test.sh
+++ tools/simt-step-test-raiser/test.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-cmake --build build -t simt-step-test-raiser
-if [ $? -ne 0 ]; then
-    exit
-fi
+# cmake --build build -t simt-step-test-raiser
+# if [ $? -ne 0 ]; then
+#     exit
+# fi
 
 files=${1:-*}
 lang=${2:-"glsl-amber"}
@@ -27,27 +27,30 @@ for testfile in tools/simt-step-test-raiser/tests/$files.mlir; do
 
     noext="${testfile%.*}"
     yamlflag=$( [ ! -f $noext.yaml ] || echo "--buffer-init-yaml $noext.yaml" && echo )
-    ./build/tools/simt-step-test-raiser/simt-step-test-raiser $testfile -o $outfile --mlir-to-$lang $yamlflag --no-f64
+    ./build/tools/simt-step-test-raiser/simt-step-test-raiser $testfile --mlir-to-$lang $yamlflag --no-f64 -o $outfile
+    # ./build/tools/simt-step-raise/simt-step-raise $testfile --target $lang > $outfile
     if [ $? -ne 0 ]; then
         continue
     fi
 
-    if [ $lang == "glsl-amber" ]; then
-        if [ "$3" == "waterthrush" ]; then
-            scp $outfile skagle@waterthrush.be.ucsc.edu:~/testout.amber
-            cat ../pass | ssh skagle@waterthrush.be.ucsc.edu sudo -S /home/skagle/amber/out/Debug/amber -D 0 testout.amber
-        else
-            sudo -S /home/simonk/amber/out/Debug/amber -D $3 $outfile
+    if [ "$3" != "none" ]; then
+        if [ $lang == "glsl-amber" ]; then
+            if [ "$3" == "waterthrush" ]; then
+                scp $outfile skagle@waterthrush.be.ucsc.edu:~/testout.amber
+                cat ../pass | ssh skagle@waterthrush.be.ucsc.edu sudo -S /home/skagle/amber/out/Debug/amber -D 0 testout.amber
+            else
+                sudo -S /home/simonk/amber/out/Debug/amber -D $3 $outfile
+            fi
+        elif [ $lang == "cuda" ]; then
+            nvcc -w $outfile && sudo ./a.out
+        elif [ $lang == "hip" ]; then
+            scp $outfile skagle@waterthrush.be.ucsc.edu:~/testout.hip
+            ssh skagle@waterthrush.be.ucsc.edu hipcc -w testout.hip
+            cat ../pass | ssh skagle@waterthrush.be.ucsc.edu sudo -S ./a.out
         fi
-    elif [ $lang == "cuda" ]; then
-        nvcc -w $outfile && sudo ./a.out
-    elif [ $lang == "hip" ]; then
-        scp $outfile skagle@waterthrush.be.ucsc.edu:~/testout.hip
-        ssh skagle@waterthrush.be.ucsc.edu hipcc -w testout.hip
-        cat ../pass | ssh skagle@waterthrush.be.ucsc.edu sudo -S ./a.out
-    fi
-    if [ $? -ne 0 ]; then
-        echo "Output in: $outfile"
-        continue
+        if [ $? -ne 0 ]; then
+            echo "Output in: $outfile"
+            continue
+        fi
     fi
 done
